c393540cda8912c1fad8d3f814204b6727c22006
kzahel <kgraehl@gmail.com>
2012-09-10 16:18:47 -0700

    use threadpool for sha1 hashing

--- js/bittorrent.threadhasher.js
+++ js/bittorrent.threadhasher.js
@@ -1,26 +1,42 @@
 (function() {
-
     jstorrent.ThreadHasher = function() {
-        this.worker = new Worker('../js/bittorrent.hasher.worker.js');
+        //this.worker = new Worker('../js/bittorrent.hasher.worker.js');
+        this.workers = [];
+        this.numthreads = 4;
+        for (var i=0; i<this.numthreads; i++) {
+            var worker = new Worker('../js/bittorrent.hasher.worker.js');
+            this.workers.push( worker );
+            worker.addEventListener('message', _.bind(this.onmessage,this,worker));
+            worker.addEventListener('error', _.bind(this.onerror,this,worker));
+        }
         this.requests = {};
         this.msgid = 0;
-        this.worker.addEventListener('message', _.bind(this.onmessage,this));
-        this.worker.addEventListener('error', _.bind(this.onerror,this));
     }
-
     jstorrent.ThreadHasher.prototype = {
+        get_worker: function() {
+            for (var i=0; i<this.numthreads; i++) {
+                if (! this.workers[i].processing) {
+                    return this.workers[i];
+                }
+            }
+            var i = Math.floor(Math.random() * this.numthreads);
+            return this.workers[i];
+        },
         send: function(data, callback) {
+            worker = this.get_worker();
+            worker.processing = true;
             this.requests[this.msgid] = callback
             data.id = this.msgid;
-            this.worker.postMessage(data);
+            worker.postMessage(data);
             this.msgid++;
         },
-        onmessage: function(msg) {
+        onmessage: function(worker, msg) {
+            worker.processing = false;
             mylog(LOGMASK.hash,'got worker message',msg);
             var callback = this.requests[msg.data.id];
             callback(msg.data);
         },
-        onerror: function(data) {
+        onerror: function(worker, data) {
             mylog(LOGMASK.error,'worker error');
             debugger;
         }

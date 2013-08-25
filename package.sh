b449a86346d9f0939d6ba889e7c94db36032971c
Kyle Graehl <kgraehl@spotify.com>
2013-08-25 09:50:49 -0700

    tweak package script

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 rm package.zip
-zip package.zip -r * -x package.sh -x *.git* -x "*.*~" -x extension
+zip package.zip -r * -x package.sh -x *.git* -x "*.*~" -x extension -x "*chrome-platform-analytics*" -x "*polyfill*" -x output -x "extension/*" -x manifest.json.scratch
